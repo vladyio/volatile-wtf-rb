f1df68588dd68b8b1af9aa168ab91e15570327f5
Uladzislau Andreyeu <me@vld.by>
2019-11-17 14:08:34 +0300

    Add nicer Cache spec descriptions

--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe Volatile::Cache do
     expect(cache.storage).to eq({})
   end
 
-  it "successfully runs #save for a key-value pair" do
+  it "#save" do
     cache.save('key', 'value')
     expect(cache.storage).to eq({'key' => 'value'})
   end
 
-  it "successfully runs #fetch for an existing key" do
+  it "#fetch for an existing key" do
     cache.save('key', 'value')
     value = cache.fetch('key')
     expect(value).to eq('value')
   end
 
-  it "yields block result passed to #fetch for an absent key" do
+  it "#fetch for an absent key" do
     value = cache.fetch('totally_does_not_exist') { 'default' }
     expect(value).to eq('default')
   end
