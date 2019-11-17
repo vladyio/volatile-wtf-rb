c82db6167fad6827fded9fe58c52ba37b3746776
Uladzislau Andreyeu <me@vld.by>
2019-11-17 13:48:32 +0300

    Add BASE_URL spec

--- spec/volatile_wtf_spec.rb
+++ spec/volatile_wtf_spec.rb
@@ -5,3 +5,9 @@ RSpec.describe VolatileWtf do
     expect(VolatileWtf::VERSION).not_to be nil
   end
 end
+
+RSpec.describe Volatile do
+  it "has BASE_URL" do
+    expect(Volatile::BASE_URL).not_to be nil
+  end
+end
